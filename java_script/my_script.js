0e721a28b355946e796f47a553eae41604740533
Claudio <claudiodicaprio89@gmail.com>
2021-11-05 16:11:43 +0100

    if_

---
+++ java_script/my_script.js
@@ -0,0 +1,26 @@
+// alert('ciao')
+let age = parseInt(prompt('your age'));
+let km = parseInt(prompt('how many km you want to travel'));
+const priceOnKm = 0.21;
+let price = priceOnKm * (km);
+let discount; 
+
+if (age < 18) {
+    
+    a = price * 20;
+    b = a / 100;
+    discount = price - b;
+}
+else if  (age > 64){
+    
+    a = price * 40;
+    b = a / 100;
+    discount = price - b;
+}
+
+else {
+    discount = price;
+}
+
+document.getElementById('ticket').innerHTML = 'price' +  discount;
+
